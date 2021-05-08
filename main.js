1d36f2ef58f46682971d0ab4139c352c963c3118
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-08 13:47:35 +0200

    Add array of students and print name and surname of each

--- main.js
+++ main.js
@@ -19,3 +19,35 @@ for (var key in studente) {
     `)
 }
 
+//Creare un array di oggetti di studenti.
+var classe = [
+    {
+        nome: 'Riccardo',
+        cognome: 'Beraldo',
+        eta: 23
+    },
+    {
+        nome: 'Michele',
+        cognome: 'Deliso',
+        eta: 28
+    },
+    {
+        nome: 'Alessandro',
+        cognome: 'Buttafoco',
+        eta: 26
+    }
+];
+
+var arrayStudentiEl = document.getElementById('array_studenti');
+var user_name = prompt('Inserisci il nome: ');
+var user_surname = prompt('Inserisci il cognome: ');
+var user_age = prompt("Inserisci l'età");
+
+//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for (var i = 0; i < classe.length; i++) {
+    arrayStudentiEl.insertAdjacentHTML('beforeend', `
+    <li> Studente ${[i + 1]}: ${classe[i].nome} ${classe[i].cognome}</li>
+    `)
+}
+
+//Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
